4c27c6b8e7813cb915a414e742087b5cade08e5e
Hector Valdes <hectorvaldes0808@gmail.com>
2022-11-25 09:56:24 -0600

    feat: install dotenv and set it up

--- next.config.js
+++ next.config.js
@@ -1,7 +1,17 @@
 /** @type {import('next').NextConfig} */
+const webpack = require('webpack')
+
+const { parsed: myEnv } = require('dotenv').config({
+  path:'.env'
+})
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  webpack(config) {
+    config.plugins.push(new webpack.EnvironmentPlugin(myEnv))
+    return config
+  }
 }
 
 module.exports = nextConfig
